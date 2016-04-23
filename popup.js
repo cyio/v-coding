01841bf4a1ddd72668ad7b9d9bcf2617a9c68945
cyio <icaner@qq.com>
2016-04-23 11:41:19 +0800

    项目添加默认分支提交历史链接

--- popup.js
+++ popup.js
@@ -1,45 +1,15 @@
 $(function() {
+  'use strict';
+  
   var vm = new Vue({
     el: '#app',
     data: {
       user: {},
       projects: {},
+      baseUrl: "https://coding.net"
     },
     computed: {},
     methods: {
-      getDefaultBranch: function(user, project) {
-        var defaultBranch;
-        this.$http.get('https://coding.net/api/user/' + user  + '/project/' + project + '/git/branches', function(result, status, request){
-          if(status == 200 && result.code === 0) {
-            // defaultBranch = result.data.list[0].name
-            console.log(result.data.list[0].name)
-          }
-        });
-        // return defaultBranch
-      },
-      getProjects: function (callback) {
-        var self = this
-        CodingAPI.projects('all', function(result) {
-          var projects = result;
-          if (!projects.code) {
-            // console.log(projects)
-            self.projects = $.map(projects, function(project) {
-              return {
-                'user': project.owner_user_name,
-                '$.path': project.project_path + '/git',
-                '$.icon': project.icon,
-                'name': project.name,
-                'https_url': project.https_url,
-                'git_url': project.git_url,
-                'isPrivate': !project.is_public,
-                'activityUpdateCount': project.un_read_activities_count || 0
-              };
-            })
-          } else {
-            console.log('请前往登陆')
-          }
-        });
-      },
       getUser: function () {
         var self = this
         CodingAPI.me(function(result) {
@@ -47,8 +17,8 @@ $(function() {
             var userData = result.data;
             self.user = {
               name: userData.name,
-              '$.avatar': userData.avatar,
-              '$.path': userData.path,
+              'avatar': userData.avatar,
+              'path': userData.path,
               'points_left': userData.points_left
             };
           }
@@ -56,8 +26,49 @@ $(function() {
       }
     },
     ready: function() {
+      var self = this
+      
       this.getUser();
-      this.getProjects()
+      
+      var getProjects = function () {
+        return new Promise (
+          function(resolve, reject) {
+            CodingAPI.projects('all', function(result) {
+              var projects = result;
+              if (!projects.code) {
+                // console.log(projects)
+                self.projects = $.map(projects, function(project) {
+                  console.log(project)
+                  return {
+                    'user': project.owner_user_name,
+                    'path': project.project_path,
+                    'icon': project.icon,
+                    'name': project.name,
+                    'https_url': project.https_url,
+                    'ssh_url': project.ssh_url,
+                    'isPrivate': !project.is_public,
+                    'activityUpdateCount': project.un_read_activities_count || 0
+                  };
+                })
+                resolve(self.projects);
+              } else {
+                reject('请前往登陆')
+              }
+            });            
+          }
+        )
+      }
+      
+      getProjects().then(function(projects){
+        $.each(projects , function(i, val) { 
+          self.$http.get('https://coding.net/api/user/' + projects[i].user  + '/project/' + projects[i].name + '/git/branches', function(result, status, request){
+            if(status == 200 && result.code === 0) {
+              projects[i].default_branch = result.data.list[0].name
+            }
+          });
+        });
+        
+      })
     }
   })
 });
