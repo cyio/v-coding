54c24743a2f01a41225ff76a6ddc9166282a9a5d
cyio <icaner@qq.com>
2017-08-23 08:17:25 +0800

    设置 react babel

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,4 +1,5 @@
-import $ from 'jquery'
+import React from 'react'
+import ReactDOM from 'react-dom';
 import axios from 'axios'
 import { mainConnector, Storage } from './modules/utils'
 
@@ -9,3 +10,44 @@ port.onMessage((msg) => {
   // console.log('frontend msg', msg)
 })
 
+// 5种状态　component + (Will, Did) + (Mount, Update, Unmount)
+// 特例是 unmound，没有 did，卸载后程序也就没了
+// 函数转换而来，拥有 state
+class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: new Date()
+    }
+  }
+
+  componentDidMount() {
+    // 临时存储给 this
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById('app'))
+// render(<h1>hello world</h1>, document.getElementById('app'))
