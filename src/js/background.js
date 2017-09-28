8cc7b6be790d16629313bc16623a21e859879f0d
cyio <icaner@qq.com>
2017-09-28 16:36:33 +0800

    fix 提示数角标

--- src/js/background.js
+++ src/js/background.js
@@ -1,14 +1,35 @@
 import { backgroundConnector, Storage } from './modules/utils'
 import { CodingAPI } from './api.js'
-import $ from 'jquery'
+
+function updateActivityCount () {
+	let num = 0
+  let notificationUnreadProjects = []
+
+  CodingAPI.getProjects()
+    .then(projects => {
+      projects.map(project => {
+        if (!!project.activityUpdateCount) {
+          num = num + 1
+          notificationUnreadProjects.push(project)
+        }
+      })
+
+      chrome.browserAction.setBadgeText({ text: num > 0 ? String(num) : '' });
+      Storage.setValue('projects', projects)
+      Storage.setValue('notificationUnreadProjects', notificationUnreadProjects)
+    })
+}
 
 chrome.alarms.create('unread', {
 	periodInMinutes: 1
 });
 
+updateActivityCount() 
+
 chrome.alarms.onAlarm.addListener((alarm) => {
 	updateActivityCount() 
 });
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	if (message === 'updateBadgeCount') {
 		setTimeout(()=>{
@@ -17,22 +38,3 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 	}
 });
 
-const updateActivityCount = () => {
-	let num = 0
-  let notificationUnreadProjects = []
-
-  CodingAPI.getProjects()
-    .then(projects => {
-      $.map(projects, (project) => {
-        if (!!project.un_read_activities_count) {
-          num = num + 1
-          notificationUnreadProjects.push(project)
-        }
-      });
-
-      chrome.browserAction.setBadgeText({ text: num > 0 ? String(num) : '' });
-      Storage.setValue('projects', projects)
-      Storage.setValue('notificationUnreadProjects', notificationUnreadProjects)
-    })
-
-}
